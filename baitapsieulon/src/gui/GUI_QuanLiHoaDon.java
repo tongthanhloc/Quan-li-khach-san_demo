a222bf29252f6000d53a4a03e93033c5ecf8121d
21094341.long@student.iuh.edu.vn <21094341.long@student.iuh.edu.vn>
2024-04-09 15:50:41 +0700

    123

---
+++ baitapsieulon/src/gui/GUI_QuanLiHoaDon.java
@@ -0,0 +1,473 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+
+
+public class GUI_QuanLiHoaDon extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	private JPanel Frame;
+	private JButton btnTK;
+    private JButton btnTKDMK;
+    private JButton btnTKDX;
+    private JButton btnTrangChu;
+    private JButton btnQLP;
+    private JButton btnQLHD;
+    private JButton btnQLKH;
+    private JButton btnQLNV;
+    private JButton btnQLKM;
+    private JButton btnQLDV;
+    private JButton btnHT;
+    private JLabel lblNewLabel_2;
+    private JLabel lblNewLabel_3;
+    private JLabel lblNewLabel_4;
+    private JLabel lblNewLabel_5;
+    private JLabel lblNewLabel_6;
+    private Panel panelTK;
+    private JLabel btnTKHTNV;
+    private JLabel btnTKTNV;
+    private JLabel btnTKca;
+    private JLabel btnmaNV;
+    private JPanel panel_Center_Top;
+    private JLabel lblMahoaDon;
+    private JLabel lblMaNhanVien;
+    private JLabel lblNgNhanPhong;
+    private JTextField txtMahoaDon;
+    private JTextField txtMaNhanVien;
+    private JTextField txtNgNhanPhong;
+    private JLabel lblKhachHang;
+    private JLabel lblNgDatPhong;
+    private JLabel lblNgTraPhong;
+    private JTextField txtMaKhachHang;
+    private JTextField txtNgDatPhong;
+    private JTextField txtNgTraPhong;
+    private JButton btbXoaTrang;
+    private JTable table;
+	private DefaultTableModel modelHD;
+	private JTable tableHD;
+	/**
+	 * Launch the application.
+	 */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GUI_QuanLiHoaDon frame = new GUI_QuanLiHoaDon();
+                    frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Phóng to cửa sổ JFrame
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI_QuanLiHoaDon() {
+		setIconImage(new ImageIcon(dangnhap.class.getResource("/img/logo.png")).getImage().getScaledInstance(100,100, java.awt.Image.SCALE_SMOOTH));
+		setTitle("Quản lý khách sạn");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0,0,1920,1000);
+		setLocationRelativeTo(null);
+		Frame = new JPanel();
+		Frame.setBackground(new Color(255, 255, 255));
+		Frame.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(Frame);
+		Frame.setLayout(null);
+
+		
+		panelTK = new Panel();
+		panelTK.setBounds(1647, 53, 247, 218);
+		Frame.add(panelTK);
+		panelTK.setLayout(null);
+		panelTK.setVisible(false);
+		
+       
+		
+		btnTKDMK = new JButton("Đổi mật khẩu");
+		btnTKDMK.setBounds(0, 141, 247, 39);
+		panelTK.add(btnTKDMK);
+		btnTKDMK.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnTKDMK.setForeground(new Color(244, 244, 244));
+		btnTKDMK.setBackground(new Color(41, 139, 106));
+		
+		btnTKDX = new JButton("<html><div style='text-align: center;'>Đăng xuất</div></html>");
+		btnTKDX.setBounds(0, 179, 247, 39);
+		panelTK.add(btnTKDX);
+		btnTKDX.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnTKDX.setForeground(new Color(244, 244, 244));
+		btnTKDX.setBackground(new Color(41, 139, 106));
+		
+		btnTKHTNV = new JLabel("Họ tên nhân viên");
+		btnTKHTNV.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnTKHTNV.setHorizontalAlignment(SwingConstants.CENTER);
+		btnTKHTNV.setBounds(0, 26, 247, 20);
+		panelTK.add(btnTKHTNV);
+		
+		btnTKTNV = new JLabel("Tuổi");
+		btnTKTNV.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnTKTNV.setHorizontalAlignment(SwingConstants.CENTER);
+		btnTKTNV.setBounds(0, 50, 247, 20);
+		panelTK.add(btnTKTNV);
+		
+		btnTKca = new JLabel("Ca làm");
+		btnTKca.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnTKca.setHorizontalAlignment(SwingConstants.CENTER);
+		btnTKca.setBounds(0, 69, 247, 20);
+		panelTK.add(btnTKca);
+		
+		btnmaNV = new JLabel("Mã nhân viên");
+		btnmaNV.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnmaNV.setHorizontalAlignment(SwingConstants.CENTER);
+		btnmaNV.setBounds(0, 0, 247, 29);
+		panelTK.add(btnmaNV);
+		
+		Panel panel_top = new Panel();
+		panel_top.setBackground(new Color(192, 192, 192));
+		panel_top.setBounds(0, 0, 1904, 150);
+		Frame.add(panel_top);
+		panel_top.setLayout(null);
+		
+		JLabel logo = new JLabel("");
+		logo.setHorizontalAlignment(SwingConstants.CENTER);
+		logo.setIcon(new ImageIcon(new ImageIcon(dangnhap.class.getResource("/img/logo.png")).getImage().getScaledInstance(200, 120, java.awt.Image.SCALE_SMOOTH)));
+		logo.setBounds(0, 0, 250, 150);
+		panel_top.add(logo);
+		
+		
+		btnTK = new JButton("<html><div style='text-align: center;'>Trần ngu</div></html>");
+		btnTK.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnTK.setForeground(new Color(244, 244, 244));
+		btnTK.setBackground(new Color(41, 139, 106));
+		btnTK.setBounds(1647, 11, 247, 40);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(new ImageIcon(dangnhap.class.getResource("/img/account-icon.png")).getImage().getScaledInstance(35,35, java.awt.Image.SCALE_SMOOTH)));
+		btnTK.add(lblNewLabel);
+		panel_top.add(btnTK);
+		
+		JButton btnTim_1 = new JButton("Thống Kê doanh thu");
+		btnTim_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnTim_1.setBounds(307, 27, 334, 99);
+		panel_top.add(btnTim_1);
+		btnTim_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		
+
+		
+		Panel panel_menu = new Panel();
+		panel_menu.setLayout(null);
+		panel_menu.setBackground(Color.LIGHT_GRAY);
+		panel_menu.setBounds(0, 150, 250, 821);
+		Frame.add(panel_menu);
+		
+		
+		btnTrangChu = new JButton("Trang chủ");
+		btnTrangChu.setBackground(new Color(255, 255, 255));
+		btnTrangChu.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnTrangChu.setBounds(0, 0, 250, 70);
+		panel_menu.add(btnTrangChu);
+		
+		
+		btnQLP = new JButton("Quản lí phòng");
+		btnQLP.setBackground(new Color(255, 255, 255));
+		btnQLP.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLP.setBounds(0, 68, 250, 68);
+		panel_menu.add(btnQLP);
+		
+		
+		btnQLHD = new JButton("Quản lí hóa đơn");
+		btnQLHD.setBackground(new Color(55, 149, 128));
+		btnQLHD.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLHD.setBounds(0, 133, 250, 68);
+		panel_menu.add(btnQLHD);
+		
+		
+		btnQLKH = new JButton("Quản lí Khách hàng");
+		btnQLKH.setBackground(new Color(255, 255, 255));
+		btnQLKH.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLKH.setBounds(0, 198, 250, 68);
+		panel_menu.add(btnQLKH);
+		
+		
+		btnQLNV = new JButton("Quản lí nhân viên");
+		btnQLNV.setBackground(new Color(255, 255, 255));
+		btnQLNV.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLNV.setBounds(0, 261, 250, 68);
+		panel_menu.add(btnQLNV);
+		
+		btnQLKM = new JButton("Quản lí khuyến mãi");
+		btnQLKM.setBackground(new Color(255, 255, 255));
+		btnQLKM.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLKM.setBounds(0, 323, 250, 68);
+		panel_menu.add(btnQLKM);
+		
+		btnQLDV = new JButton("Quản lí dịch vụ");
+		btnQLDV.setBackground(new Color(255, 255, 255));
+		btnQLDV.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQLDV.setBounds(0, 388, 250, 68);
+		panel_menu.add(btnQLDV);
+		
+		btnHT = new JButton("Hỗ trợ");
+		btnHT.setBackground(new Color(255, 255, 255));
+		btnHT.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnHT.setBounds(0, 453, 250, 68);
+		panel_menu.add(btnHT);
+		
+		lblNewLabel_2 = new JLabel("__________________________________________");
+		lblNewLabel_2.setForeground(new Color(41, 111, 106));
+		lblNewLabel_2.setBounds(0, 629, 260, 19);
+		panel_menu.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("Thông tin khách sạn");
+		lblNewLabel_3.setForeground(new Color(41, 111, 106));
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(0, 654, 250, 30);
+		panel_menu.add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("Hotline: 0387272513");
+		lblNewLabel_4.setForeground(new Color(41, 111, 106));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_4.setBounds(0, 703, 250, 30);
+		panel_menu.add(lblNewLabel_4);
+		
+		lblNewLabel_5 = new JLabel("Địa chỉ: 416/39 Dương Quảng Hàm");
+		lblNewLabel_5.setForeground(new Color(41, 111, 106));
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_5.setBounds(0, 735, 250, 30);
+		panel_menu.add(lblNewLabel_5);
+		
+		lblNewLabel_6 = new JLabel("Phường 5, Gò Vấp, TP.HCM");
+		lblNewLabel_6.setForeground(new Color(41, 111, 106));
+		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_6.setBounds(0, 754, 250, 30);
+		panel_menu.add(lblNewLabel_6);
+		
+		panel_Center_Top = new JPanel();
+		panel_Center_Top.setBackground(new Color(255, 255, 255));
+		panel_Center_Top.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_Center_Top.setBounds(251, 149, 1653, 223);
+		Frame.add(panel_Center_Top);
+		panel_Center_Top.setLayout(null);
+		
+		lblMahoaDon = new JLabel("Mã hóa đơn");
+		lblMahoaDon.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMahoaDon.setBounds(140, 25, 126, 35);
+		panel_Center_Top.add(lblMahoaDon);
+		
+		lblMaNhanVien = new JLabel("Mã nhân viên");
+		lblMaNhanVien.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMaNhanVien.setBounds(140, 75, 126, 35);
+		panel_Center_Top.add(lblMaNhanVien);
+		
+		lblNgNhanPhong = new JLabel("Ngày nhận phòng");
+		lblNgNhanPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNgNhanPhong.setBounds(140, 125, 186, 35);
+		panel_Center_Top.add(lblNgNhanPhong);
+		
+		txtMahoaDon = new JTextField();
+		txtMahoaDon.setBackground(new Color(55, 149, 128));
+		txtMahoaDon.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtMahoaDon.setBounds(350, 25, 350, 40);
+		panel_Center_Top.add(txtMahoaDon);
+		txtMahoaDon.setColumns(10);
+		
+		txtMaNhanVien = new JTextField();
+		txtMaNhanVien.setBackground(new Color(55, 149, 128));
+		txtMaNhanVien.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtMaNhanVien.setColumns(10);
+		txtMaNhanVien.setBounds(350, 73, 350, 40);
+		panel_Center_Top.add(txtMaNhanVien);
+		
+		txtNgNhanPhong = new JTextField();
+		txtNgNhanPhong.setBackground(new Color(55, 149, 128));
+		txtNgNhanPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtNgNhanPhong.setColumns(10);
+		txtNgNhanPhong.setBounds(350, 125, 350, 40);
+		panel_Center_Top.add(txtNgNhanPhong);
+		
+		lblKhachHang = new JLabel("Mã khách hàng");
+		lblKhachHang.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblKhachHang.setBounds(900, 25, 210, 35);
+		panel_Center_Top.add(lblKhachHang);
+		
+		lblNgDatPhong = new JLabel("Ngày đặt phòng");
+		lblNgDatPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNgDatPhong.setBounds(900, 75, 175, 35);
+		panel_Center_Top.add(lblNgDatPhong);
+		
+		lblNgTraPhong = new JLabel("Ngày trả phòng");
+		lblNgTraPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNgTraPhong.setBounds(900, 125, 198, 35);
+		panel_Center_Top.add(lblNgTraPhong);
+		
+		txtMaKhachHang = new JTextField();
+		txtMaKhachHang.setBackground(new Color(55, 149, 128));
+		txtMaKhachHang.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtMaKhachHang.setColumns(10);
+		txtMaKhachHang.setBounds(1100, 25, 350, 40);
+		panel_Center_Top.add(txtMaKhachHang);
+		
+		txtNgDatPhong = new JTextField();
+		txtNgDatPhong.setBackground(new Color(55, 149, 128));
+		txtNgDatPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtNgDatPhong.setColumns(10);
+		txtNgDatPhong.setBounds(1100, 75, 350, 40);
+		panel_Center_Top.add(txtNgDatPhong);
+		
+		txtNgTraPhong = new JTextField();
+		txtNgTraPhong.setBackground(new Color(55, 149, 128));
+		txtNgTraPhong.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtNgTraPhong.setColumns(10);
+		txtNgTraPhong.setBounds(1100, 125, 350, 40);
+		panel_Center_Top.add(txtNgTraPhong);
+		
+		JButton btnTim = new JButton("Tìm");
+		btnTim.setBounds(1160, 176, 175, 35);
+		panel_Center_Top.add(btnTim);
+		btnTim.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnTim.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		btbXoaTrang = new JButton("Xóa trắng");
+		btbXoaTrang.setBounds(1389, 176, 175, 35);
+		panel_Center_Top.add(btbXoaTrang);
+		btbXoaTrang.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		JPanel panel_Center_Bot = new JPanel();
+		panel_Center_Bot.setBackground(new Color(255, 255, 255));
+		panel_Center_Bot.setBounds(256, 374, 1648, 576);
+
+		
+		String[] cols = new String[] {"Mã hóa đơn", "Mã nhân viên", "Mã khách hàng", "Mã Khuyến mãi ", "Ngày lập hóa đơn", "Tổng tiền thanh toán" ,"Tiền khách đưa" , "Tiền thối", "Trang thái"};
+		modelHD = new DefaultTableModel(cols,0);
+		panel_Center_Bot.setLayout(null);
+		tableHD = new JTable(modelHD);
+		tableHD.setBackground(new Color(128, 255, 0));
+		JScrollPane paneNV = new JScrollPane(tableHD);
+		paneNV.setBounds(10, 27, 1610, 538);
+		paneNV.setPreferredSize(new Dimension(1000,1000));
+		panel_Center_Bot.add(paneNV);;
+		JTableHeader headers = tableHD.getTableHeader();
+        Font headerFont = new Font("Tahoma", Font.PLAIN, 15);
+        headers.setFont(headerFont);
+		Frame.add(panel_Center_Bot);
+		
+		
+		
+		
+        
+		
+		
+		Frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Ẩn btnXTT và btnDX khi click ra ngoài
+                panelTK.setVisible(false);
+                // Hiển thị btnTK
+                btnTK.setVisible(true);
+            }
+        });
+	        ActionListener actionListener = new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                JButton clickedButton = (JButton) e.getSource();
+	                // Xử lý sự kiện cho mỗi nút ở đây
+	                boolean isVisible = panelTK.isVisible();
+	                if (clickedButton == btnTK && isVisible == false) {
+	                    // Xử lý khi nhấn vào nút btnTK
+	                	panelTK.setVisible(true);
+	                } else if (clickedButton == btnTK && isVisible == true) {
+	                    // Xử lý khi nhấn vào nút btnXTT
+	                	panelTK.setVisible(false);
+	                }
+	                else if (clickedButton == btnTKDMK) {
+	                    // Xử lý khi nhấn vào nút btnXTT
+	                } else if (clickedButton == btnTKDX) {
+	                    // Xử lý khi nhấn vào nút btnDX
+	                	int option = JOptionPane.showConfirmDialog(null, "Bạn có muốn đăng xuất không?", "Xác nhận đăng xuất", JOptionPane.YES_NO_OPTION);
+                        if (option == JOptionPane.YES_OPTION) {
+	                	setVisible(false); // Đóng frame hiện tại
+	                    new dangnhap().setVisible(true);
+	                }} else if (clickedButton == btnTrangChu) {
+                        // Xử lý khi nhấn vào nút btnTrangChu
+	                	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLP) {
+                        // Xử lý khi nhấn vào nút btnQLP
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLHD) {
+                        // Xử lý khi nhấn vào nút btnQLHD
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLKH) {
+                        // Xử lý khi nhấn vào nút btnQLKH
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLNV) {
+                        // Xử lý khi nhấn vào nút btnQLNV
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLKM) {
+                        // Xử lý khi nhấn vào nút btnQLKM
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnQLDV) {
+                        // Xử lý khi nhấn vào nút btnQLDV
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    } else if (clickedButton == btnHT) {
+                        // Xử lý khi nhấn vào nút btnHT
+                    	btnTKDMK.setVisible(false);
+	                    btnTKDX.setVisible(false);
+                    }}};
+                    btnTK.addActionListener(actionListener);
+                    btnTKDMK.addActionListener(actionListener);
+                    btnTKDX.addActionListener(actionListener);
+                    btnTrangChu.addActionListener(actionListener);
+                    btnQLP.addActionListener(actionListener);
+                    btnQLHD.addActionListener(actionListener);
+                    btnQLKH.addActionListener(actionListener);
+                    btnQLNV.addActionListener(actionListener);
+                    btnQLKM.addActionListener(actionListener);
+                    btnQLDV.addActionListener(actionListener);
+                    btnHT.addActionListener(actionListener);
+	}
+
+	
+
+	public JButton getBtnTK() {
+		return btnTK;
+	}
+
+	public void setBtnTK(JButton btnTK) {
+		this.btnTK = btnTK;
+	}
+
+	public JButton getBtnXTT() {
+		return btnTKDMK;
+	}
+
+	public void setBtnXTT(JButton btnXTT) {
+		this.btnTKDMK = btnXTT;
+	}
+}
